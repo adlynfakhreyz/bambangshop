ed7fb1e35c27d687162185e50db84316ce1ec167
adlynfakhreyz <adlynuniacc@gmail.com>
2025-03-28 12:19:51 +0800

    Implement publish function in Program service and Program controller.

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -15,13 +15,12 @@ pub struct Subscriber {
 impl Subscriber {
     #[tokio::main]
     pub async fn update(&self, payload: Notification) {
-        REQUEST_CLIENT
+        REQWEST_CLIENT
             .post(&self.url)
             .header("Content-Type", "JSON")
             .body(to_string(&payload).unwrap())
             .send().await.ok();
-        
-        log::warn!("Sent {} notification of: [{}] {}, to: {}",
+        log::warn_!("Sent {} notification of: [{}] {}, to: {}",
             payload.status, payload.product_type, payload.product_title, self.url);
     }
 }
